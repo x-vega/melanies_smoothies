fe976fdca873397c61a20f346ff09c5472843a37
x-vega <xvega@powerdata.cl>
2024-07-19 19:46:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("Customize Your Smoothie :balloon:")
@@ -29,6 +30,8 @@ if ingredients_list:
    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen +' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
     st.write(ingredients_string)
 
@@ -46,8 +49,6 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!'+ name_on_order, icon="✅")
         
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
+
 
