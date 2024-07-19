1b8d0127d03657350e4e842d01da4ba9857f8597
x-vega <xvega@powerdata.cl>
2024-07-19 18:14:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title("Customize Your Smoothie :balloon:")
+st.title("My Parents New Healthy Diner")
 st.write(
     """Choose the frutis you
     """)
